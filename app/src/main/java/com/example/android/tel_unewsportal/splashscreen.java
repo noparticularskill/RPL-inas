090b3f2fd99fa7ae759706f056bcec551d1ec3ed
Inas <submitboxinas@gmail.com>
2019-04-03 18:48:28 +0700

    Grid View Layout

---
+++ app/src/main/java/com/example/android/tel_unewsportal/splashscreen.java
@@ -0,0 +1,33 @@
+package com.example.android.tel_unewsportal;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+
+        CountDownTimer downTimer = new CountDownTimer(3000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Intent activity2 = new Intent(splashscreen.this, Splash.class);
+                startActivity(activity2);
+                finish();
+            }
+        };
+        downTimer.start();
+    }
+}
+
